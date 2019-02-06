a590dca9b8664e75fc148daa491f32908eedc599
zatirka <lychkovsky.an@gmail.com>
2019-02-06 19:18:37 +0300

    Using Blueprints to Discover C++

---
+++ BattleTank/Source/BattleTank/Tank.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank.h"
+
+// Sets default values
+ATank::ATank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	InputComponent->BindAction("Turret_clockwise", IE_Pressed, this, &ATank::RotateTurret);
+}
+
+void ATank::RotateTurret()
+{
+	UE_LOG(LogTemp, Warning, TEXT("RotateTurret Called"));
+	// SetRelativeRotation
+	// UChildActorComponent
+}
