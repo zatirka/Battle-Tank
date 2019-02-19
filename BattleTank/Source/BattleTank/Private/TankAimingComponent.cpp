447fc6908eb5167bf9ae94a738442f7c061632d8
zatirka <lychkovsky.an@gmail.com>
2019-02-19 18:10:36 +0300

    Review Execution Flow

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -39,9 +39,14 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	if(bHaveAimSolution)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
-		
-		MoveBarrelTowards(AimDirection);
-		
+		MoveBarrelTowards(AimDirection);	
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
+	}
+	else
+	{
+		auto Time = GetWorld()->GetTimeSeconds();
+		UE_LOG(LogTemp, Warning, TEXT("%f: No aim solution found"), Time);
 	}
 	// If no solution found do nothing
 }
--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -29,9 +29,6 @@ protected:
 	virtual void BeginPlay() override;
 
 private:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
